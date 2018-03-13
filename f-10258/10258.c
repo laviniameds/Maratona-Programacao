be4cad620b1075a6c5575b5ae04ab6b8ac12d7f7
Lavinia Medeiros <laviniameds@icloud.com>
2018-03-13 10:00:53 -0300

    ->10258 not OK;

---
+++ f-10258/10258.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+int main(int argc, char const *argv[]) {
+
+  int qtd;
+
+  scanf("%d", &qtd);
+
+  while(qtd != 0){
+
+    while (scanf("%d", &num) != "\n") {
+      
+    }
+
+    qtd--;
+  }
+
+  return 0;
+}
